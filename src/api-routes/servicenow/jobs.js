03023f91f715783877dc0292f5cdb37d19cebfe9
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2022-10-08 15:50:55 -0700

    WIP - minor fix

--- src/api-routes/servicenow/jobs.js
+++ src/api-routes/servicenow/jobs.js
@@ -1,6 +1,19 @@
-import { errorHandler } from 'errors/errors';
+import _ from 'lodash';
+
+import { errorHandler, errorBuilder } from 'errors/errors';
 import { postJob } from '../../db/oracledb/servicenow-dao';
 import { serializeJob } from '../../serializers/jobs-serializer';
+
+/**
+ * Helper function to build error
+ *
+ * @type {RequestHandler}
+ */
+const buildErrors = (res, err) => {
+  const { statusCode, message } = err;
+
+  return errorBuilder(res, statusCode, [message]);
+};
 
 /**
  * Create job
@@ -14,6 +27,15 @@ const post = async (req, res) => {
     const result = serializeJob(rawJob);
     return res.status(202).send(result);
   } catch (err) {
+    if (_.startsWith(err, 'Error: ORA-20100: ::ID does not exist.')) {
+      err.statusCode = 400;
+      err.message = 'ID does not exist.';
+      return buildErrors(res, err);
+    }
+
+    if (err.statusCode) {
+      return buildErrors(res, err);
+    }
     return errorHandler(res, err);
   }
 };
